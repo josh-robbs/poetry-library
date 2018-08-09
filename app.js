b218c2a2c1a9d1e6a71d01d1c409ff6335e47aea
Ciaran <cguhagil@terpmail.umd.edu>
2018-08-09 09:43:53 -0600

    pushing app.js code to cheeran branch i hope

---
+++ app.js
@@ -0,0 +1,13 @@
+function makeShitHappen() {
+
+    let blarp = document.getElementById('div-3');
+
+    const werds = document.createElement('p');
+
+    werds.textContent = 'blahblahnipples'
+
+    blarp.appendChild(werds)
+
+}
+
+makeShitHappen()
